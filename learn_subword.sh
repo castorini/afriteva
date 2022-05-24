956b859a16a00028a45c5b439664922864a218c6
Ogundepo Odunayo <ogundepoodunayo@gmail.com>
2022-05-23 23:35:11 -0400

    Update learn_subword.sh

--- learn_subword.sh
+++ learn_subword.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-#SBATCH --mem=64G
-#SBATCH --cpus-per-task=10
-#SBATCH --time=24:00:0
-#SBATCH --gres=gpu:a100:1
 
-#SBATCH -o logs/learn_tokenizer_am_ha_sw.log
-
-# export CUDA_AVAILABLE_DEVICES=0
+export CUDA_AVAILABLE_DEVICES=0
 data_path=$1
 vocab_size=$2
 
