f151f50236023f90fa8c201695255a4d8a6c8261
Ogundepo Odunayo <ogundepoodunayo@gmail.com>
2023-01-10 21:24:46 -0500

    change floating precision

--- classification_scripts/classification_trainer.py
+++ classification_scripts/classification_trainer.py
@@ -123,7 +123,7 @@ def main():
         accelerator='gpu' if torch.cuda.is_available() else 'cpu',
         devices=args.n_gpu if args.n_gpu else 1,
         max_epochs=args.num_train_epochs,
-        precision=16 if args.fp_16 else 32,
+        precision=32,
         gradient_clip_val=args.max_grad_norm,
         callbacks=[LoggingCallback(), checkpoint_callback],
         logger=csv_logger
